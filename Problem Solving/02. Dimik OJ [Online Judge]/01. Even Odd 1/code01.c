d114e1fe25b04c9d7ff8885f58998539dadbead1
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-06-28 09:34:58 +0600

    problem1 solved

---
+++ Problem Solving/02. Dimik OJ [Online Judge]/01. Even Odd 1/code01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int T;
+    unsigned int N;
+    scanf("%d", &T);
+    if(1<=T<=100){
+        while(T!=0){
+            scanf("%d", &N);
+            if(N%2==0){
+                printf("even\n");
+            }else{
+                printf("odd\n");
+            }
+            T--;
+        }
+    }
+}
