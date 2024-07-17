62c3f06b438d51e28ea928127d85d6ced38c43d8
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-07-17 23:58:56 +0600

    multiplication of two matrix

--- Problem Solving/04. Topic Wise Problem Solve/Array/Problem10.c
+++ Problem Solving/04. Topic Wise Problem Solve/Array/Problem10.c
@@ -1,2 +1,26 @@
 // Write a C program to draw a multiplication table using two dimensional array. 
 
+#include<stdio.h>
+int main(){
+    int mtx1[2][2] = {{1,2},{1,2}};
+    int mtx2[2][2] = {{1,2},{1,2}};
+    int mtx_mul[2][2];
+
+    int i,j,k;
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            for(k=0;k<2;k++){
+                mtx_mul[j][k] = mtx1[i][k] * mtx2[k][j];
+            }
+        }
+    }
+
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            printf("%d ", mtx_mul[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
