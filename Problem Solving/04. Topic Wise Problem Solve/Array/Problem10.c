951cf92b4eda22dce49b9f6c01946ff4fc0edc60
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-07-18 00:19:29 +0600

    2*2 matrix multiplication

--- Problem Solving/04. Topic Wise Problem Solve/Array/Problem10.c
+++ Problem Solving/04. Topic Wise Problem Solve/Array/Problem10.c
@@ -6,12 +6,15 @@ int main(){
     int mtx2[2][2] = {{1,2},{1,2}};
     int mtx_mul[2][2];
 
-    int i,j,k;
+    int i,j,k,sum = 0;
     for(i=0;i<2;i++){
         for(j=0;j<2;j++){
+             
             for(k=0;k<2;k++){
-                mtx_mul[j][k] = mtx1[i][k] * mtx2[k][j];
+               sum = sum + (mtx1[i][k] * mtx2[k][j]);
             }
+            mtx_mul[i][j] = sum;
+            sum = 0;
         }
     }
 
