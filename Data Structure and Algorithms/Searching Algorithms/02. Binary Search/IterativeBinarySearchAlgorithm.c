790bed286c7df627f72475110e101056ea1dd02b
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-10-03 01:16:56 +0600

    iterative binary search method

---
+++ Data Structure and Algorithms/Searching Algorithms/02. Binary Search/IterativeBinarySearchAlgorithm.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int binarySearch(int arr[], int low, int high, int x){
+    while(high>=low){
+        int mid = low + (high - low) / 2;
+        if(arr[mid]==x){
+            return mid;
+        }
+        if(arr[mid]>x){
+            high = mid -1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 20;
+    int result = binarySearch(arr, 0, n - 1, x);
+
+    if (result==-1){
+        printf("Element is not present in array.");
+    }
+    else{
+        printf("Element is present at index %d.", result);
+    }
+
+    return 0;
+}
