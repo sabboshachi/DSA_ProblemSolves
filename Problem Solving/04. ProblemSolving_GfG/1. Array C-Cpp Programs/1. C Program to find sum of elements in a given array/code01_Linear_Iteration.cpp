afbb2a4b16de75be47aa4050bd32beaeab457a37
Sabboshachi Sarkar <55670542+sabboshachi@users.noreply.github.com>
2024-11-17 07:55:48 +0600

    sum of elements of an array using linear iteration

---
+++ Problem Solving/04. ProblemSolving_GfG/1. Array C-Cpp Programs/1. C Program to find sum of elements in a given array/code01_Linear_Iteration.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a[], int n){
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        sum = sum + a[i];
+    }
+    return sum;
+}
+
+int main(){
+    int arr[] = {12, 3, 4, 15};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Sum of the elements of the array is: " << sum(arr, size);
+    return 0;
+}
